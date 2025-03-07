3a123b9bf171c4f09efdd87fe8fb14758b47879e
MenichettiDev <menichetti.dev@gmail.com>
2025-03-07 18:14:45 -0300

    /api a todas las rutas

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const authController = require('../controllers/authController'); //Importamos el controlador para manejar la autenticación
 
 //Ruta para mostrar la página de inicio de sesión
-router.get('/login', authController.getLogin); 
+router.get('/api/login', authController.getLogin); 
 
 //Ruta para redirigir a los usuarios a la página de inicio de sesión si acceden a la raíz "/"
-router.get('/', authController.redirectLogin);
+router.get('/api/', authController.redirectLogin);
 
 //Ruta para manejar el envío del formulario de inicio de sesión
-router.post('/login', authController.postLogin); 
+router.post('/api/login', authController.postLogin); 
 
 //Ruta para cerrar sesión del usuario
-router.post('/logout', authController.logout); //Llamamos al método logout para cerrar la sesión
+router.post('/api/logout', authController.logout); //Llamamos al método logout para cerrar la sesión
 
 module.exports = router; //Exportamos el router para poder usarlo en otras partes de la aplicación
