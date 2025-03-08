eac6fd33c286b98248d3f7ae6691743af27a992d
MenichettiDev <menichetti.dev@gmail.com>
2025-03-07 22:39:27 -0300

    Revert "probando cosas, cualquier cosa borrar"
    This reverts commit 39d43f8ceefa9eaa7debf53e7f2a0ee6e8c07170.

--- app.js
+++ app.js
@@ -8,34 +8,13 @@ const authRoutes = require('./routes/authRoutes'); //Importamos las rutas de aut
 const bodyParser = require('body-parser'); //Middleware para analizar el cuerpo de las solicitudes HTTP.
 const cors = require('cors');
 
-//errores
-const fs = require('fs');
-const morgan = require('morgan'); // Instala con: npm install morgan
-
-// Crear archivo de logs
-const logStream = fs.createWriteStream(
-  path.join(__dirname, 'logs', 'api.log'), 
-  { flags: 'a' }
-);
-
-// Registrar todas las peticiones
-app.use(morgan('combined', { stream: logStream }));
-
-// Middleware para errores
-app.use((err, req, res, next) => {
-  console.error('❌ ERROR:', err.stack); // Muestra errores en consola
-  logStream.write(`❌ [${new Date().toISOString()}] ${err.stack}\n`); // Guarda en archivo
-  res.status(500).json({ error: 'Internal Server Error' });
-});
-//////////////////////////////////////////////////////////
-
 
 //Sesiones
 const session = require('express-session'); //Middleware para gestionar sesiones de usuario.
 
 app.use(express.json()); //Si necesitas manejar JSON en tu aplicación.
 
-// Configuración de express-session
+//Configuración de express-session
 app.use(session({
   secret: 'claveSecreta', //Cambia por una clave secreta más segura.
   resave: false, //No volver a guardar la sesión si no ha sido modificada.
@@ -48,23 +27,19 @@ app.use(session({
 const allowedOrigins = [
   'http://localhost:4200',
   'https://localhost:4200',
-  'https://gymrats.com.ar', // Dominio frontend
-  'https://www.gymrats.com.ar' // Variante con www
+  'https://gymrats.com.ar' // Asegúrate de incluir HTTPS si es necesario
 ];
 
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error(`CORS bloqueado: ${origin}`));
-    }
+  origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true); // Permite el origen
+      } else {
+          callback(new Error('Not allowed by CORS')); // Bloquea el origen
+      }
   },
-  credentials: true // ¡Importante para cookies/sesiones!
+  credentials: true // Permitir el envío de cookies o encabezados con credenciales
 }));
-
-// ¡Antes de las rutas!
-app.set('trust proxy', true); // Confía en el proxy (OpenLiteSpeed)
 
 // Middleware para manejar solicitudes OPTIONS
 app.options('*', cors()); // Habilita CORS para todas las rutas y métodos OPTIONS
