130a1a36e67bb22ce6bf5d06a2935278d550ae13
MenichettiDev <menichetti.dev@gmail.com>
2025-03-15 00:17:08 -0300

    debug db

--- config/conexion.js
+++ config/conexion.js
@@ -13,6 +13,11 @@ const conexion = mysql.createConnection({
 
 // Verificación de la conexión
 conexion.connect((error) => {
+    console.log('DB_HOST:', process.env.DB_HOST);
+    console.log('DB_PORT:', process.env.DB_PORT);
+    console.log('DB_USER:', process.env.DB_USER);
+    console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+    console.log('DB_DATABASE:', process.env.DB_DATABASE);
     if (error) {
         console.error('Error al conectar a la base de datos:', error.stack);
         return;
