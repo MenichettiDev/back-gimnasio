1ee49ac100ad57e6ea9e3e662a8d43d1692616fa
MenichettiDev <menichetti.dev@gmail.com>
2025-03-24 20:19:21 -0300

    refactor: pool a FraseService

--- services/fraseService.js
+++ services/fraseService.js
@@ -1,18 +1,31 @@
 const conexion = require('../config/conexion');
 
-
+// Obtener una frase aleatoria
 const fraseAleatoria = () => {
     return new Promise((resolve, reject) => {
-        const queryFraseAleatoria = `SELECT * FROM tb_frases ORDER BY RAND() LIMIT 1`; 
-        conexion.query(queryFraseAleatoria, (error, resultados) => {
-            if (error) return reject(error);
-            resolve(resultados);  // Solo devolvemos la primera frase (única fila aleatoria)
+        // Obtener una conexión del pool
+        conexion.getConnection((err, connection) => {
+            if (err) return reject(err);
+
+            const queryFraseAleatoria = `
+                SELECT * 
+                FROM tb_frases 
+                ORDER BY RAND() 
+                LIMIT 1
+            `;
+
+            // Ejecutar la consulta
+            connection.query(queryFraseAleatoria, (error, resultados) => {
+                // Liberar la conexión después de usarla
+                connection.release();
+
+                if (error) return reject(error);
+                resolve(resultados); // Devuelve la primera frase (única fila aleatoria)
+            });
         });
     });
 };
 
-
 module.exports = {
     fraseAleatoria
-
 };
